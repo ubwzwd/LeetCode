cbc52cda6c06f06fe89f1fe8bb758a71fb8e5c9a
ubwzwd <ubwzwd@gmail.com>
2019-01-04 00:27:19 -0500

    Create 142.cpp

---
+++ C++/142.cpp
@@ -0,0 +1,35 @@
+// Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+// To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
