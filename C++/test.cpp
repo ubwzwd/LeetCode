5363904b9d09422a70b5548417bea92c052149e1
ubwzwd <ubwzwd@gmail.com>
2019-10-20 17:37:57 -0400

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -2,17 +2,31 @@
 #include <iostream>
 using namespace std;
 
+class Solution {
+    void probRec(vector<double>& prob, int target, double& res, double& temp, int index){
+        if(index == prob.size() - 1){
+            if(target > 0) temp += res*prob[index];
+            else temp += res*(1-prob[index]);
+            return;
+        }
+        if(target > 0){
+            res *= prob[index];
+            probRec(prob, target - 1, res, temp, index + 1);
+            res /= prob[index];
+        }
+        res *= (1-prob[index]);
+        probRec(prob, target, res, temp, index + 1);
+    }
+public:
+    double probabilityOfHeads(vector<double>& prob, int target) {
+        double res = 1, temp = 0;
+        probRec(prob, target, res, temp, 0);
+        return temp;
+    }
+};
+
 int main()
 {
-    vector<vector<int>> dp;
-    for(int i = 0; i < 10; i++)
-    {
-        vector<int> temp {1};
-        dp.push_back(temp);
-    }
-    for(auto i : dp)
-    {
-        cout<<i[0];
-    }
+
     return 0;
 }
