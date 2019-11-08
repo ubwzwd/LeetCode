21a63ee4d58a13c7676e1f171787c25c2a85fc80
ubwzwd <ubwzwd@gmail.com>
2019-11-08 14:03:36 -0500

    Create 246.strobogrammatic-number.cpp

---
+++ C++/246.strobogrammatic-number.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=246 lang=cpp
+ *
+ * [246] Strobogrammatic Number
+ *
+ * https://leetcode.com/problems/strobogrammatic-number/description/
+ *
+ * algorithms
+ * Easy (43.15%)
+ * Likes:    153
+ * Dislikes: 373
+ * Total Accepted:    62.5K
+ * Total Submissions: 143.9K
+ * Testcase Example:  '"69"'
+ *
+ * A strobogrammatic number is a number that looks the same when rotated 180
+ * degrees (looked at upside down).
+ * 
+ * Write a function to determine if a number is strobogrammatic. The number is
+ * represented as a string.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:  "69"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:  "88"
+ * Output: true
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input:  "962"
+ * Output: false
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isStrobogrammatic(string num) {
+        
+    }
+};
+// @lc code=end
+
