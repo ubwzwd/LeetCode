7e797000d7e9778fa2fb219ec2bbe8a7961ac8d4
ubwzwd <ubwzwd@gmail.com>
2019-11-07 21:13:34 -0500

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,18 +1,40 @@
-#include <vector>
 #include <iostream>
+#include <string>
+#include <stack>
 using namespace std;
 
-int main()
-{
-    vector<vector<int>> dp;
-    for(int i = 0; i < 10; i++)
-    {
-        vector<int> temp {1};
-        dp.push_back(temp);
+int main(){
+    string s;
+    getline(cin, s);
+    if(s.size() <= 2){
+        cout << s << endl;
+        return 0;
     }
-    for(auto i : dp)
-    {
-        cout<<i[0];
+    int left = 0;
+    int i = 0;
+    int flag = 0;
+    string temp;
+    while(i < s.size()){
+        if(s[i] == '['){
+            left ++;
+        }
+        else if(s[i] != ']'){
+            if(flag == 0 && s[i] != ','){
+                temp.push_back('[');
+                flag = 1;
+            }
+            temp.push_back(s[i]);
+        }
+        else{
+            left --;
+            if(temp.empty())
+                cout << '[' <<']';
+            else
+                cout << temp << ']';
+            temp.clear();
+            flag = 0;
+        }
+        i++;
     }
     return 0;
 }
