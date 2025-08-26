911b83ee84d2f6196e6fdb5355d078a0d33e603c
ubwzwd <ubwzwd@DESKTOP-NC1CDOQ.localdomain>
2025-08-26 22:35:14 +0800

    leetcode 129 go

---
+++ Go/129.sum-root-to-leaf-numbers.go
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode id=129 lang=golang
+ *
+ * [129] Sum Root to Leaf Numbers
+ *
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+ *
+ * algorithms
+ * Medium (68.87%)
+ * Likes:    8481
+ * Dislikes: 150
+ * Total Accepted:    1.2M
+ * Total Submissions: 1.7M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * You are given the root of a binary tree containing digits from 0 to 9 only.
+ *
+ * Each root-to-leaf path in the tree represents a number.
+ *
+ *
+ * For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+ *
+ *
+ * Return the total sum of all root-to-leaf numbers. Test cases are generated
+ * so that the answer will fit in a 32-bit integer.
+ *
+ * A leaf node is a node with no children.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,2,3]
+ * Output: 25
+ * Explanation:
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Therefore, sum = 12 + 13 = 25.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [4,9,0,5,1]
+ * Output: 1026
+ * Explanation:
+ * The root-to-leaf path 4->9->5 represents the number 495.
+ * The root-to-leaf path 4->9->1 represents the number 491.
+ * The root-to-leaf path 4->0 represents the number 40.
+ * Therefore, sum = 495 + 491 + 40 = 1026.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [1, 1000].
+ * 0 <= Node.val <= 9
+ * The depth of the tree will not exceed 10.
+ *
+ *
+ */
+package main
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+var sum int
+
+func sumNumbers(root *TreeNode) int {
+	sum = 0
+	sumNumbersHelper(root, 0)
+	return sum
+}
+
+func sumNumbersHelper(node *TreeNode, currentSum int) {
+	if node == nil {
+		return
+	}
+	currentSum = currentSum*10 + node.Val
+	if node.Left == nil && node.Right == nil {
+		sum += currentSum
+		return
+	}
+	sumNumbersHelper(node.Left, currentSum)
+	sumNumbersHelper(node.Right, currentSum)
+}
+
+// @lc code=end
