fe2f28e7331c2f669a7f787feb6d45f7a0f7c85d
wudi.zheng <wudi.zheng@grabtaxi.com>
2026-01-13 18:05:26 +0800

    mac-2

---
+++ C++/Users/wudi.zheng/Downloads/LeetCode/C++/1987.number-of-unique-good-subsequences.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1987 lang=cpp
+ *
+ * [1987] Number of Unique Good Subsequences
+ */
+#include <string>
+using namespace std;
+// @lc code=start
+class Solution
+{
+private:
+    int backtrackingSolution(string binary)
+    {
+    }
+
+public:
+    int numberOfUniqueGoodSubsequences(string binary)
+    {
+    }
+};
+// @lc code=end
